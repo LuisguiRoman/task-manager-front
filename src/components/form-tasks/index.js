7c26e57ad8ae268288f864e99cb425e253ef67d8
Luisgui <luisguirohi@gmail.com>
2020-01-19 16:47:43 -0500

    Formulario de creacion de tareas

---
+++ src/components/form-tasks/index.js
@@ -0,0 +1,111 @@
+// DEPENDENCIES
+import React, { useState } from 'react';
+import axios from 'axios';
+import M from 'materialize-css';
+
+//constantes
+import { api_url, endpoints, auth_token } from '../../constants';
+
+// STYLES
+import './form-task.scss';
+
+export const FormTasks = props =>{
+    //Estado del componente
+    const [create, setCreate] = useState({
+        name: '',
+        priority: '',
+        date: ''
+    });
+    const [disabled, setDisabled] = useState(false);
+
+    const handleChange = event =>{
+        setCreate({
+            ...create,
+            [event.target.name]: event.target.value
+        });
+    }
+
+    const handleSubmit = event =>{
+        event.preventDefault();
+        const valid = handleValidate();
+        if(valid){
+            createTask();
+        }
+    }
+
+    const handleValidate = () =>{
+        console.log(create);
+
+        if(create.name === '' || create.priority === '' || create.date === ''){
+            M.Toast.dismissAll();
+            M.toast({ html: 'Completa todos los campos', classes: 'error'});
+            return false;
+        }
+        return true;
+    }
+
+    const createTask = () =>{
+        setDisabled(true);
+
+        const DATA = {
+            task_name: create.name,
+            priority: parseInt(create.priority),
+            expiration_date: new Date(create.date)
+        };
+
+        console.log(DATA);
+
+        axios
+            .post(`${api_url}${endpoints.tasks}/create`, DATA, {
+                headers: { 'Authorization': `Bearer ${auth_token}` }
+            })
+            .then(res => {
+                console.log(res.data.data);
+                props.addTask(res.data.data);
+
+                M.toast({ html: 'Tarea creada', classes: 'success' });
+                setCreate({name: '', priority: '', date: ''});
+                setDisabled(false);
+            })
+            .catch(error => {
+                console.log(error, error.response);
+                M.toast({ html: 'Ocurrio un error', classes: 'error' });
+                setDisabled(false);
+            });
+    }
+
+    const { name, priority, date } = create;
+
+    return (
+        <div id="form-tasks" className="modal">
+            <div className="modal-content">
+                <div className="container-form-task">
+                    <h3 className="text-center">Crea una nueva tarea</h3>
+                    <form autoComplete="off" className="row" onSubmit={handleSubmit} noValidate>
+                        <div className="input-field col-12">
+                            <input id="name" name="name" type="text" value={name} onChange={handleChange} />
+                            <label htmlFor="name">Nombre</label>
+                        </div>
+                        <div className="input-field col-12">
+                            <select className="browser-default" name="priority" value={priority} onChange={handleChange}>
+                                <option value="" disabled>Prioridad</option>
+                                <option value="1">Prioridad alta</option>
+                                <option value="2">Prioridad media</option>
+                                <option value="3">Prioridad baja</option>
+                            </select>
+                        </div>
+                        <div className="input-field col-12">
+                            <input type="date" id="date" name="date" value={date} onChange={handleChange} />
+                            <label htmlFor="date">Fecha de vencimiento: </label>
+                        </div>
+                        <div className="input-field col-12 text-center">
+                            <button className="btn waves-effect" type="submit" disabled={disabled}>
+                                {disabled ? 'Creando...': 'Crear tarea'}
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/components/page-dashboard/index.js
+++ src/components/page-dashboard/index.js
@@ -1,11 +1,86 @@
 // DEPENDENCIES
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState, useContext } from 'react';
+import axios from 'axios';
+import M from 'materialize-css';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+//CONTEXT
+import { AppContext } from '../../context';
+
+//constantes
+import { api_url, endpoints, auth_token } from '../../constants';
+
+//COMPONENTS
+import { FormTasks } from '../form-tasks';
+
+// STYLES
+import './dashboad.scss';
 
 export const Dashboard = () =>{
+    //Descomponer el contexto y obtener las propiedades
+    const { app_state: {name}, app_logout } = useContext(AppContext);
+    
+    //Estado inicial
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(()=>{
+        let elems = document.querySelectorAll('.modal');
+        M.Modal.init(elems);
+        getTasks();
+    }, []);
+
+    const getTasks = () =>{
+        axios
+            .post(`${api_url}${endpoints.tasks}`, {}, {
+                headers: { 'Authorization': `Bearer ${auth_token}` }
+            })
+            .then(res => {
+                //Redirigir al dashboard
+                console.log(res.data.data.tasks);
+                setTasks(res.data.data.tasks);
+            })
+            .catch(error => {
+                console.log(error, error.response);
+                //M.toast({ html: 'Ocurrio un error', classes: 'error' });
+            });
+    }
+
+    const showModal = () =>{
+        const elem = document.getElementById('form-tasks');
+        let instance = M.Modal.getInstance(elem);
+        instance.open();
+    }
+
+    const handleAddTask = new_task =>{
+        const task_list = tasks;
+        task_list.push(new_task);
+        setTasks(task_list);
+    }
+
+    //obtener la primera palabra del string
+    const first_name = name.replace(/ .*/,'');
+
     return (
-        <div id="page-home" className="container">
+        <Fragment>
+            <header>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-lg-7">
+                            <h2>Hola {first_name}!</h2>
+                        </div>
+                        <div className="col-12 col-lg-5 text-right">
+                            <button type="button" className="btn waves-effect" onClick={showModal}>Crear tarea</button>
+                            <button type="button" className="btn waves-effect close-session" onClick={app_logout}>Cerrar sesión</button>
+                        </div>
+                    </div>
+                </div>
+            </header>
 
+            <div id="page-dashboard" className="container">
+            </div>
 
-        </div>
+            <FormTasks addTask={handleAddTask} />
+
+        </Fragment>
     );
 }
